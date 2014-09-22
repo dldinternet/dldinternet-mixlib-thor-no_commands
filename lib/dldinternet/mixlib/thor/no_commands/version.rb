fffcc278729120c0f169df754f03b090f5a27eab
Christo DeLange <cdelange@amplify.com>
2014-09-22 09:15:22 -0400

    Bump version

--- lib/dldinternet/mixlib/thor/no_commands/version.rb
+++ lib/dldinternet/mixlib/thor/no_commands/version.rb
@@ -3,7 +3,7 @@ module Dldinternet
     module Thor
       module Nocommands
         # dldinternet-mixlib-thor-nocommands version
-        VERSION = "0.1.0"
+        VERSION = '0.1.1'
       end
     end
   end
