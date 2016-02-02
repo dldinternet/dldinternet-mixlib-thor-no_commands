fe72fa5ed9cb08c9f68389be540def8603f77834
Christo De Lange <christo_delange@homedepot.com>
2016-02-02 10:58:50 -0500

    Newer ruby and dependencies

--- lib/dldinternet/mixlib/thor/no_commands/version.rb
+++ lib/dldinternet/mixlib/thor/no_commands/version.rb
@@ -3,7 +3,7 @@ module Dldinternet
     module Thor
       module Nocommands
         # dldinternet-mixlib-thor-nocommands version
-        VERSION = '0.3.0'
+        VERSION = '0.3.1'
       end
     end
   end
