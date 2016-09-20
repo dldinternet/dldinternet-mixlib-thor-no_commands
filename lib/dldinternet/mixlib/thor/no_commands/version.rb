6f2a0f44074f20bfc0b7fd05d17bdc58da3151ea
Christo De Lange <christo_de_lange@homedepot.com>
2016-09-20 12:00:47 -0400

    Bump version and support MAJOR,MINOR

--- lib/dldinternet/mixlib/thor/no_commands/version.rb
+++ lib/dldinternet/mixlib/thor/no_commands/version.rb
@@ -3,7 +3,18 @@ module Dldinternet
     module Thor
       module Nocommands
         # dldinternet-mixlib-thor-nocommands version
-        VERSION = '0.4.1'
+        VERSION = '0.4.2'
+
+        module Version # :nodoc: all
+          MAJOR, MINOR, RELEASE, *OTHER = VERSION.split '.'
+
+          NUMBERS = [MAJOR, MINOR, RELEASE, *OTHER].freeze
+        end
+
+        # Returns the version string for the library.
+        def self.version
+          VERSION
+        end
       end
     end
   end
